e9bf53411a9572345a1a34289d639befffb9f2e9
Jeremy Chauvin <jeremie.chauvin@epsi.fr>
2020-06-08 01:42:01 +0200

    format

--- controllers/products.ts
+++ controllers/products.ts
@@ -49,57 +49,70 @@ const getProduct = (
   }
 };
 
-const addProduct = async ({ request, response }: { request: any, response: any }) => {    
-    const body = await request.body()
+const addProduct = async (
+  { request, response }: { request: any; response: any },
+) => {
+  const body = await request.body();
 
-    if (!request.hasBody) {
-        response.status = 400
-        response.body = {
-            success: false,
-            msg: 'No data'
-        }
-    } else {
-        const product: Product = body.value
-        product.id = v4.generate()
-        products.push(product)
-        response.status = 201
-        response.body = {
-            success: true,
-            data: product
-        }
-    }
-}
+  if (!request.hasBody) {
+    response.status = 400;
+    response.body = {
+      success: false,
+      msg: "No data",
+    };
+  } else {
+    const product: Product = body.value;
+    product.id = v4.generate();
+    products.push(product);
+    response.status = 201;
+    response.body = {
+      success: true,
+      data: product,
+    };
+  }
+};
 
-const updateProduct = async({ params, request, response }: { params: { id: string }, request: any, response: any }) => {
-    const product: Product | undefined = products.find(p => p.id === params.id)
+const updateProduct = async (
+  { params, request, response }: {
+    params: { id: string };
+    request: any;
+    response: any;
+  },
+) => {
+  const product: Product | undefined = products.find((p) => p.id === params.id);
 
-    if (product) {
-        const body = await request.body()
+  if (product) {
+    const body = await request.body();
 
-        const updateData: { name?: string; description?: string; price?: number } = body.value
+    const updateData: { name?: string; description?: string; price?: number } =
+      body.value;
 
-        products = products.map(p => p.id === params.id ? { ...p, ...updateData } : p)
+    products = products.map((p) =>
+      p.id === params.id ? { ...p, ...updateData } : p
+    );
 
-        response.status = 200
-        response.body = {
-            success: true,
-            data: products
-        }
-    } else {
-        response.status = 404
-        response.body = {
-            success: false,
-            msg: 'No product found'
-        }
-    }
-}
+    response.status = 200;
+    response.body = {
+      success: true,
+      data: products,
+    };
+  } else {
+    response.status = 404;
+    response.body = {
+      success: false,
+      msg: "No product found",
+    };
+  }
+};
 
-const deleteProduct = ({ params, response }: { params: { id: string }, response: any }) => {
-    products = products.filter(p => p.id !== params.id)
-    response.body = { 
-        success: true,
-        msg: 'Product removed'
-    }
-}
+const deleteProduct = (
+  { params, response }: { params: { id: string }; response: any },
+) => {
+  products = products.filter((p) => p.id !== params.id);
+  response.body = {
+    success: true,
+    msg: "Product removed",
+  };
+};
 
-export { getProducts, getProduct, addProduct, updateProduct, deleteProduct }
+export { getProducts, getProduct, addProduct, updateProduct, deleteProduct };
