fbc96f68e44ef3db123e3692139061ea93ea4bf6
Jeremy Chauvin <jeremie.chauvin@epsi.fr>
2020-06-08 02:42:46 +0200

    add .env

--- server.ts
+++ server.ts
@@ -1,8 +1,10 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import { green, yellow } from "https://deno.land/std@0.53.0/fmt/colors.ts";
+import "https://deno.land/x/dotenv/load.ts";
 import router from "./routes.ts";
 import logger from "./middlewares/logger.ts";
-const port = 8000;
+
+const port = Number(Deno.env.get("PORT")) || 8000;
 
 const app = new Application();
 
